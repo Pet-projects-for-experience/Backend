e5f81ea0f9f4f2eeab7006ebb865485510a04eff
Denis Shtanskiy <denis_shtanskii@bk.ru>
2024-04-24 23:46:56 +0300

    refactor: Готовые изменения по CI/CD

--- src/backend/config/settings/dev.py
+++ src/backend/config/settings/dev.py
@@ -1,20 +1,23 @@
 from .base import *  # noqa
 
 DEBUG = True
+SERVER_HOST = getenv("SERVER_HOST_DEV")
+SERVER_NAME = getenv("SERVER_NAME_DEV")
+
 
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 
 CORS_ALLOWED_ORIGINS = [
-    "https://89.23.117.80",
-    "https://devcodepet.tw1.ru",
+    "https://SERVER_HOST",
+    "https://SERVER_NAME",
     "http://localhost:3000",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://89.23.117.80",
-    "https://devcodepet.tw1.ru",
+    "https://SERVER_HOST",
+    "https://SERVER_NAME",
 ]
 
 
