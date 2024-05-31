6a8ca8ec8275dd2e906ba02b8edc1ace68b24201
Badmajor <v.balonkin@gmail.com>
2024-05-31 12:15:00 +0300

    add send messsage

--- src/backend/apps/projects/signals.py
+++ src/backend/apps/projects/signals.py
@@ -1,7 +1,8 @@
+from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from apps.projects.models import Project
+from apps.projects.models import InvitationToProject, Project
 
 
 @receiver(post_save, sender=Project)
@@ -11,3 +12,15 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created and not instance.creator.is_organizer:
         instance.creator.is_organizer = True
         instance.creator.save()
+
+
+@receiver(post_save, sender=InvitationToProject)
+def send_invite_to_user(sender, instance, created, **kwargs):
+    """Метод отправки письма пользователю, когда его приглашают в проект"""
+    if created:
+        send_mail(
+            "Вас приглашают в проект",
+            f"Вы получили приглашение в проект {instance.project.name}",
+            "info@codepet.ru",
+            (instance.user.email,),
+        )
