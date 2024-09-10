04fc5a56c761324b40e23dbac74cc1342a4dbce8
Badmajor <v.balonkin@gmail.com>
2024-09-10 10:59:21 +0300

    add filter for username and name fields

--- src/backend/api/v1/profile/filters.py
+++ src/backend/api/v1/profile/filters.py
@@ -1,3 +1,4 @@
+from django.db.models import Case, IntegerField, Q, Value, When
 from django_filters.rest_framework import FilterSet, filters
 
 from apps.general.constants import LEVEL_CHOICES
@@ -19,6 +20,7 @@ class ProfileFilter(FilterSet):
         field_name="specialists__skills", queryset=Skill.objects.all()
     )
     is_favorite = filters.NumberFilter(method="filter_is_favorite_profile")
+    search = filters.CharFilter(method="filter_search")
 
     class Meta:
         model = Profile
@@ -34,4 +36,32 @@ class ProfileFilter(FilterSet):
         user = self.request.user
         if user.is_authenticated and value == 1:
             return queryset.filter(favorited_by=user)
+        return queryset
+
+    def filter_search(self, queryset, name, value):
+        value = value.strip()
+        if not value and len(value) < 3:
+            return queryset
+
+        exact_match = Q(name__iexact=value) | Q(user__username__iexact=value)
+        partial_match = Q(name__icontains=value) | Q(
+            user__username__icontains=value
+        )
+
+        exact_match_case = When(exact_match, then=Value(0))
+        partial_match_case = When(partial_match, then=Value(1))
+
+        queryset = (
+            queryset.annotate(
+                match_priority=Case(
+                    exact_match_case,
+                    partial_match_case,
+                    default=Value(2),
+                    output_field=IntegerField(),
+                )
+            )
+            .filter(exact_match | partial_match)
+            .order_by("match_priority", "user_id")
+        )
+
         return queryset
