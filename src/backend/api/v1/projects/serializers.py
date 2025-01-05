641c70b1ec11547a1d6096f2d493d10b881151f4
denis_shtanskii <denis_shtanskii@bk.ru>
2025-01-05 17:44:27 +0300

    refactor: Fix issues from profile.about and cover_letter

--- src/backend/api/v1/projects/serializers.py
+++ src/backend/api/v1/projects/serializers.py
@@ -375,6 +375,11 @@ class MyRequestsSerializer(CustomModelSerializer):
             "project",
         )
 
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["cover_letter"] = html.unescape(rep["cover_letter"])
+        return rep
+
     def get_position(self, obj) -> str:
         """Метод получения specialization из ProfessionSerializer."""
         return ProfessionSerializer(obj.position.profession).data[
@@ -395,6 +400,14 @@ class UpdateParticipationRequestSerializer(
     class Meta:
         model = ParticipationRequest
         fields: ClassVar[Tuple[str, ...]] = ("cover_letter",)
+
+    def validate_cover_letter(self, value):
+        escaped_html = bleach.clean(
+            value,
+            tags=ALLOWED_TAGS_BY_FRONT,
+            attributes=ALLOWED_ATTRIBUTES_BY_FRONT,
+        )  # защита потенциально вредоносных HTML-тегов и атрибутов
+        return escaped_html
 
 
 class WriteParticipationRequestSerializer(
@@ -562,10 +575,10 @@ class ReadListParticipationRequestSerializer(
         )
         read_only_field = ("request_participants",)
 
-        def to_representation(self, instance):
-            rep = super().to_representation(instance)
-            rep["cover_letter"] = instance.cover_letter
-            return rep
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["cover_letter"] = html.unescape(rep["cover_letter"])
+        return rep
 
     def get_request_status(self, obj) -> str:
         """Метод получения статуса запроса."""
